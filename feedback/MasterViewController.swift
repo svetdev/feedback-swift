e862a957ca39dbd4b0ed20e510ba003036cfeff4
Andrey Kasatkin <a@svetliy.com>
2016-12-14 13:06:06 -0500

    added feedback view controller

--- feedback/MasterViewController.swift
+++ feedback/MasterViewController.swift
@@ -20,8 +20,8 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.leftBarButtonItem = self.editButtonItem
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        self.navigationItem.rightBarButtonItem = addButton
+        //let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+        //self.navigationItem.rightBarButtonItem = addButton
         if let split = self.splitViewController {
             let controllers = split.viewControllers
             self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
---
+++ feedback/WebViewController.swift
@@ -0,0 +1,14 @@
+//
+//  WebViewController.swift
+//  feedback
+//
+//  Created by Andrey Kasatkin on 12/14/16.
+//  Copyright © 2016 Svetliy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WebViewController: UIViewController {
+
+}
