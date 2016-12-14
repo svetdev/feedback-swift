5a2a26c58b30daabaf73e22e36d3b44f306e7a30
Andrey Kasatkin <a@svetliy.com>
2016-12-14 13:26:43 -0500

    loading webView

--- feedback/WebViewController.swift
+++ feedback/WebViewController.swift
@@ -8,7 +8,42 @@
 
 import Foundation
 import UIKit
+import WebKit
 
-class WebViewController: UIViewController {
+class WebViewController: UIViewController, WKNavigationDelegate {
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addWebView()
+        loadURL()
+    }
+    
+    func addWebView() {
+        webView = WKWebView()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(webView!)
+        webView.navigationDelegate = self
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|-[webView]-|",
+                                                           options: NSLayoutFormatOptions(rawValue: 0),
+                                                           metrics: nil,
+                                                           views: ["webView": webView]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-20-[webView]-|",
+                                                           options: NSLayoutFormatOptions(rawValue: 0),
+                                                           metrics: nil,
+                                                           views: ["webView": webView]))
 
+    }
+    
+    func loadURL() {
+        let urlString = "https://svetliy.herokuapp.com/contact"
+        guard let url = NSURL(string: urlString) else {return}
+        let request = NSMutableURLRequest(url:url as URL)
+        webView.load(request as URLRequest)
+    }
 }
